245840f015a4e4204b7902f0d7ba61905fd5dfc0
Jose Granados <josepgs0929@gmail.com>
2023-03-16 17:58:53 -0600

    HOTFIX: add inserts

---
+++ scripts/files_x_locations.sql
@@ -0,0 +1,12 @@
+--Para los archivos de cada playa:
+
+INSERT INTO files_x_locations (playa_id, file_id) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 4),
+(2, 5),
+(2, 6),
+(3, 7),
+(3, 8),
+(3, 9);
---
+++ scripts/locations.sql
@@ -0,0 +1,13 @@
+--Para la tabla las locaciones:
+
+INSERT INTO locations (location_name, coordinate, deleted) VALUES
+('Playa Conchal', POINT(10.3779, -85.8212), 0),
+('Playa Flamingo', POINT(10.4398, -85.7835), 0),
+('Playa Tamarindo', POINT(10.2996, -85.8394), 0),
+('Playa Jacó', POINT(9.6170, -84.6286), 0),
+('Playa Montezuma', POINT(9.6492, -85.0669), 0),
+('Playa Santa Teresa', POINT(9.6509, -85.1682), 0),
+('Playa Bonita', POINT(9.9565, -83.0294), 0),
+('Playa Negra', POINT(10.5695, -83.5153), 0),
+('Playa Cahuita', POINT(9.7429, -82.8507), 0);
+

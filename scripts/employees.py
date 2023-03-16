82069597ec95fc57bbf317bdbf8316db94f352da
S77VEN <stivensg313131@outlook.com>
2023-03-15 21:13:16 -0600

    hotfix: employees script birth dates fix

--- scripts/employees.py
+++ scripts/employees.py
@@ -7,24 +7,24 @@ db = mysql.connector.connect(
   host="localhost",
   user="root",
   password="Hailtotheking31*",
-  database="mydb"
+  database="caso2"
 )
 # generate 100,000 rows of data
 data = []
 for i in range(15):
     first_name = fake.name()
-    age = random.randint(18, 65)
+    birth_date = fake.date_of_birth(minimum_age=18, maximum_age=65)
     last_name = fake.last_name()
     domain = fake.random_element(elements=("gmail.com", "outlook.com", "hotmail.com"))
     email = f"{first_name.lower()}.{last_name.lower()}@{domain}".replace(" ", "")
     phone = fake.random_number(digits=8)
     iban_account = fake.iban()
-    data.append((i+1, first_name, last_name, phone, email, iban_account))
+    data.append((i+1, birth_date, first_name, last_name, phone, email, iban_account))
 
 # prepare the insert statement
-stmt = "INSERT INTO employees (employee_id, first_name, last_name, phone, email, iban_account) VALUES (%s, %s, %s, %s, %s, %s)"
+stmt = "INSERT INTO employees (employee_id, birth_date, first_name, last_name, phone, email, iban_account) VALUES (%s, %s, %s, %s, %s, %s, %s)"
 cursor = db.cursor()
-
+  
 # insert the data in batches of 1000
 batch_size = 1000
 for i in range(0, len(data), batch_size):

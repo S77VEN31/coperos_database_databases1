9a1bf335c2b1049804db63929f119393ca268451
S77VEN <stivensg313131@outlook.com>
2023-03-16 18:21:56 -0600

    Add: inserts for location taxes

---
+++ scripts/location_taxes.sql
@@ -0,0 +1,11 @@
+INSERT INTO location_taxes (location_id, location_tax)
+VALUES
+(1, 0.05), -- el porcentaje de impuestos para Playa Conchal podría ser del 5%
+(2, 0.03), -- el porcentaje de impuestos para Playa Flamingo podría ser del 3%
+(3, 0.07), -- el porcentaje de impuestos para Playa Tamarindo podría ser del 7%
+(4, 0.02), -- el porcentaje de impuestos para Playa Jacó podría ser del 2%
+(5, 0.04), -- el porcentaje de impuestos para Playa Montezuma podría ser del 4%
+(6, 0.06), -- el porcentaje de impuestos para Playa Santa Teresa podría ser del 6%
+(7, 0.01), -- el porcentaje de impuestos para Playa Bonita podría ser del 1%
+(8, 0.08), -- el porcentaje de impuestos para Playa Negra podría ser del 8%
+(9, 0.03); -- el porcentaje de impuestos para Playa Cahuita podría ser del 3%

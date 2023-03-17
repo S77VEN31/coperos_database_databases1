c232fd2a0dd10993a1cb8ecdfc9af15edabe8521
Jose Granados <josepgs0929@gmail.com>
2023-03-16 18:03:08 -0600

    ADD: inserts de la tabla files

---
+++ scripts/files.sql
@@ -0,0 +1,12 @@
+--Para los inserts de files:
+
+INSERT INTO files (source, deleted, file_type_id) VALUES
+('https://www.govisitcostarica.co.cr/images/photos/mob-shells-on-beach-playa-conchal.jpg', 0, 22),
+('https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/31/26/b2/entire-flamingo-beach.jpg?w=700&h=-1&s=1', 0, 22),
+('https://upload.wikimedia.org/wikipedia/commons/c/cc/Costa_Rica_Playa_Tamarindo_and_Rivermouth_2007_Aerial_Photograph_Tamarindowiki_01.JPG', 0, 22),
+('https://images.visitarcostarica.com/playa-jaco-costa-rica.jpg', 0, 22),
+('https://goplaya.cr/archivos/MONTEZUMAGOPLAYA.JPG', 0, 22),
+('https://goplaya.cr/blog/wp-content/uploads/2020/10/santateresa12-1024x576.jpg', 0, 22),
+('https://img2.10bestmedia.com/Images/Photos/218959/p-limonbeach_55_660x440_201405311241.jpg', 0, 22),
+('https://www.puertoviejosatellite.com/albums/dougdo_playa_negra.jpg', 0, 22),
+('https://www.creescapes.com/wp-content/uploads/beach-cahuita-costa-rica.jpg', 0, 22);

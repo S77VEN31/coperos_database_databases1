2d67028453bd4278671698935c43ffb8aaf770a3
Jose Granados <josepgs0929@gmail.com>
2023-03-22 20:00:31 -0600

    ADD: fix transaction_date

--- stored procedures/sales.sql
+++ stored procedures/sales.sql
@@ -4,6 +4,7 @@ CREATE PROCEDURE generate_sales()
 BEGIN
     DECLARE num_sales INT;
     DECLARE i INT DEFAULT 0;
+    DECLARE z DATETIME;
 
     DROP TEMPORARY TABLE IF EXISTS temp_work_schedule_logs;
     CREATE TEMPORARY TABLE temp_work_schedule_logs AS SELECT log_date, start_time, end_time, work_schedule_log_id FROM work_schedule_logs;
@@ -16,6 +17,7 @@ BEGIN
         SET i = 0;
 
         WHILE i < num_sales DO
+			SET z = (SELECT CONCAT(@log_date, ' ', SEC_TO_TIME(FLOOR(RAND() * (TIME_TO_SEC(@end_time) - TIME_TO_SEC(@start_time) + 1)) + TIME_TO_SEC(@start_time))));	
             INSERT INTO sales(transaction_date,
                               invoice_creation_date,
                               pay_type,
@@ -24,10 +26,8 @@ BEGIN
                               client_id,
                               work_schedule_log_id)
             VALUES(
-            
-                                (SELECT CONCAT(@log_date, ' ', SEC_TO_TIME(FLOOR(RAND() * (TIME_TO_SEC(@end_time) - TIME_TO_SEC(@start_time) + 1)) + TIME_TO_SEC(@start_time)))),
-
-                @log_date,
+				z,
+                (SELECT ADDTIME(z, CONCAT('00:00:', FLOOR(RAND() * 31) + 10))),
                 CASE 
                     WHEN RAND() < 0.6 THEN 0
                     WHEN RAND() >= 0.6 AND RAND() < 0.9 THEN 2
@@ -45,5 +45,6 @@ BEGIN
     END WHILE;
 END $$
 DELIMITER ;
+
 
 CALL generate_sales();

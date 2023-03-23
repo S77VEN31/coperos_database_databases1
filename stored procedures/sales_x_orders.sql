cecb7eb56ed544511de3bf2cb184e7c407537c42
S77VEN <stivensg313131@outlook.com>
2023-03-23 17:29:22 -0600

    Hotfix: procedure without cursors

--- stored procedures/sales_x_orders.sql
+++ stored procedures/sales_x_orders.sql
@@ -3,40 +3,25 @@ DROP PROCEDURE IF EXISTS insert_sale_from_order;
 DELIMITER $$
 CREATE PROCEDURE insert_sale_from_order()
 BEGIN
-    DECLARE done INT DEFAULT FALSE;
-    DECLARE o_id, o_client_id, o_work_schedule_log_id INT;
-    DECLARE o_creation_date DATE;
-    DECLARE cur CURSOR FOR SELECT order_id, order_creation_date, client_id, work_schedule_log_id FROM caso2.purchase_orders WHERE state = 1;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-
-    OPEN cur;
-
-    read_loop: LOOP
-        FETCH cur INTO o_id, o_creation_date, o_client_id, o_work_schedule_log_id;
-        IF done THEN
-            LEAVE read_loop;
-        END IF;
-
-        INSERT INTO caso2.sales (transaction_date,
-                                 invoice_creation_date,
-                                 pay_type,
-                                 `change`,
-                                 total_price,
-                                 client_id,
-                                 work_schedule_log_id,
-                                 commission_id)
-        SELECT o_creation_date,
-               o_creation_date,
-               1,
-               0,
-               0,
-               o_client_id,
-               o_work_schedule_log_id,
-               commission_logs.commission_id
-          FROM caso2.commission_logs
-         WHERE commission_logs.commission_log_start_date < o_creation_date AND commission_logs.commission_log_end_date >= o_creation_date;
-    END LOOP;
-
-    CLOSE cur;
+    INSERT INTO caso2.sales (transaction_date,
+                             invoice_creation_date,
+                             pay_type,
+                             `change`,
+                             total_price,
+                             client_id,
+                             work_schedule_log_id,
+                             commission_id)
+    SELECT purchase_orders.order_creation_date,
+           purchase_orders.order_creation_date,
+           1,
+           0,
+           0,
+           purchase_orders.client_id,
+           purchase_orders.work_schedule_log_id,
+           commission_logs.commission_id
+      FROM caso2.purchase_orders
+      JOIN caso2.commission_logs
+        ON commission_logs.commission_log_start_date < purchase_orders.order_creation_date AND commission_logs.commission_log_end_date >= purchase_orders.order_creation_date
+     WHERE purchase_orders.state = 1;
 END$$
 DELIMITER ;

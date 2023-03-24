8ea024317f1a04ad7f3c6ad049a3967bf78437d4
S77VEN <stivensg313131@outlook.com>
2023-03-23 19:53:03 -0600

    add: retos

---
+++ selects/reto_3.sql
@@ -0,0 +1,18 @@
+
+-- demuestre que es posible crear una consulta o varias para saber cuánto se gasto,
+-- cuánto se ganó por semana en todo el negocio,
+-- además de saber cuánto se le debe pagar a cada copero.
+
+-- los gastos de items se pueden filtar en la tabla inventory_logs
+
+-- Ventas por semana
+SELECT YEARWEEK(transaction_date) AS week,
+       MIN(transaction_date) AS week_start,
+       MAX(transaction_date) AS week_end,
+       SUM(total_price) AS total_earnings
+  FROM caso2.sales
+ WHERE deleted = 0
+ GROUP BY YEARWEEK(transaction_date)
+ ORDER BY YEARWEEK(transaction_date);
+
+ -- cuánto se le debe pagar a cada copero se consulta en employee_salary_logs

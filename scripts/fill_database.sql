d0982c876ab6269180f88588b48ab1fd2ab88bea
S77VEN <stivensg313131@outlook.com>
2023-03-23 21:02:32 -0600

    Hotfix: estapicha

--- scripts/fill_database.sql
+++ scripts/fill_database.sql
@@ -951,36 +951,4 @@ DELIMITER ;
 		CALL insert_inventory_logs(4,5,4,1,CURDATE());
 */
 
-
--- Procedure para ejecutar una instrucción por parejo cada item
-DELIMITER $$
-CREATE PROCEDURE insert_inventory_logs_all_items (
-    IN p_employee_id INT,
-    IN p_amount DECIMAL(10,2),
-    IN p_movement_type INT
-)
-BEGIN
-    DECLARE v_item_id INT;
-    DECLARE done INT DEFAULT FALSE;
-    DECLARE cur_items CURSOR FOR SELECT item_id FROM items;
-    
-    -- declarar el cursor y abrirlo
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-    OPEN cur_items;
-    
-    -- recorrer cada fila del cursor y llamar al stored procedure
-    read_loop: LOOP
-        FETCH cur_items INTO v_item_id;
-        IF done THEN
-            LEAVE read_loop;
-        END IF;
-        
-        CALL insert_inventory_logs(v_item_id, p_employee_id, p_amount, p_movement_type, CURDATE());
-    END LOOP;
-    
-    -- cerrar el cursor
-    CLOSE cur_items;
-END$$
-DELIMITER ;
-
 CALL insert_inventory_logs_all_items(1, 50000, 2);

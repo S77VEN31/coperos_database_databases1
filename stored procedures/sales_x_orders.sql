f14e6a6a76151b0b48d6630593c349c1caef3d9a
S77VEN <stivensg313131@outlook.com>
2023-03-23 18:26:43 -0600

    Hotfix: sales_x_orders fix

--- stored procedures/sales_x_orders.sql
+++ stored procedures/sales_x_orders.sql
@@ -1,5 +1,3 @@
-DROP PROCEDURE IF EXISTS insert_sale_from_order;
-
 DELIMITER $$
 CREATE PROCEDURE insert_sale_from_order()
 BEGIN
@@ -11,17 +9,24 @@ BEGIN
                              client_id,
                              work_schedule_log_id,
                              commission_id)
-    SELECT purchase_orders.order_creation_date,
-           purchase_orders.order_creation_date,
+    SELECT transaction_date_value,
+           TIMESTAMPADD(SECOND, FLOOR(RAND() * (20 - 10 + 1) + 10), transaction_date_value),
            1,
            0,
            0,
-           purchase_orders.client_id,
-           purchase_orders.work_schedule_log_id,
-           commission_logs.commission_id
-      FROM caso2.purchase_orders
-      JOIN caso2.commission_logs
-        ON commission_logs.commission_log_start_date < purchase_orders.order_creation_date AND commission_logs.commission_log_end_date >= purchase_orders.order_creation_date
-     WHERE purchase_orders.state = 1;
+           client_id,
+           work_schedule_log_id,
+           commission_id
+      FROM (SELECT DATE_FORMAT(TIMESTAMPADD(SECOND, -FLOOR(RAND() * TIMESTAMPDIFF(SECOND, work_schedule_logs.start_time, work_schedule_logs.end_time)), purchase_orders.order_creation_date), '%Y-%m-%d %H:%i:00') AS transaction_date_value,
+                   purchase_orders.client_id AS client_id,
+                   purchase_orders.work_schedule_log_id AS work_schedule_log_id,
+                   commission_logs.commission_id AS commission_id
+              FROM caso2.purchase_orders
+              JOIN caso2.commission_logs
+                ON commission_logs.commission_log_start_date < purchase_orders.order_creation_date AND commission_logs.commission_log_end_date >= purchase_orders.order_creation_date
+              JOIN caso2.work_schedule_logs
+                ON work_schedule_logs.work_schedule_log_id = purchase_orders.work_schedule_log_id
+             WHERE purchase_orders.state = 1) AS subq;
 END$$
+
 DELIMITER ;
